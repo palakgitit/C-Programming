ddb46628bde4a4624a4f9d03e70bb4de74d6b066
palakgitit <palkzzzy@gmail.com>
2024-08-31 23:07:16 +0530

    auto update

---
+++ pro2/loop practice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main ()
+
+{
+    int A , B ,i;
+ printf("enter your number of column:");
+ scanf ("%d",&A);
+ printf("enter your row:");
+ scanf("%d",&B);
+
+ do (i=1,A<=50,i++)
+ {
+    printf("*");
+
+ }
+ }
