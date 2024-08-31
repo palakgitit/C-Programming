fc7b2b94eaa575d9f55f63fffae999550a6d60b5
palakgitit <palkzzzy@gmail.com>
2024-08-31 10:04:44 +0530

    update

--- pro2/test1.c
+++ pro2/test1.c
@@ -1,1 +1,14 @@
-hello
+#include <stdio.h>
+
+void main() 
+{
+    float F, C ; 
+
+    printf (" What is the temperature: ");
+    scanf ("%f",&C);
+
+    F = C * 9/5 + 32 ; 
+
+    printf ("%f",F); 
+
+}
